e661b3545dcfc2317f6f394cfdc99489ba789c3d
Agustin <agustinescuderoweb@gmail.com>
2024-06-16 20:59:58 -0300

    Nuevo Commit

--- src/Componentes/hero.tsx
+++ src/Componentes/hero.tsx
@@ -8,11 +8,6 @@ function hero() {
   return (
     <div className={styles.container}>
         <div className={styles.img}><Image src={imagen}></Image></div>
-        <div className={styles.propuesta}>
-          <h1>Desarrollo Web</h1>
-          <p>Desarrollos web ajustables a tus necesidades actuales</p>
-          <button className={styles.boton}>Ver Planes y Precios</button>
-        </div>
     </div>
   )
 }
--- src/Componentes/planes.tsx
+++ src/Componentes/planes.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import fle from '../../public/flechazo.png'
 import Image from 'next/image'
 import styles from '@/Componentes/planes.module.css'
+import x from '../../public/icon/x-circle-fill.svg'
+import check from '../../public/icon/check-circle-fill.svg'
 
 function planes() {
   return (
@@ -13,23 +15,38 @@ function planes() {
     <div className={styles.servicios}>
         <div className={styles.landing}>
             <h1>Landing Page</h1>
-            <p>Landing Page, o página de aterrizaje es una página ideal para presentarte, ofrecer un curso gratuito o un Ebook, y a cambio solicitar sus contactos para luego lograr una conexión 
-               Es una pagina simple para que tus visitantes puedan conocer en profundidad tu conocimientos y lo que estarías dispuesto a ofrecer.</p>
+            <ul className={styles.lista}>
+            <li><Image src={check}></Image>Formulario de Contacto</li>
+            <li><Image src={check}></Image>Multiple páginas</li>
+            <li><Image src={check}></Image>Whatsaap Personalizado</li>
+            <li><Image src={check}></Image>Responsive Desing</li>
+            <li><Image src={check}></Image>Dominio Personalizado</li>
+            <li><Image src={check}></Image>Contactos Redes Sociales</li>
+            </ul>
             <h2>$60000</h2>
         </div>
         <div className={styles.web}>
             <h1>Página Web</h1>
-            <p>Una Página web es una página que te presenta tus productos o servicios de una manera directa, contiene diferentes apartados para compartir tus conocimientos, y diferentes opciones
-                presentar tus productos. Contiene un whatsaap adaptable para que pueda conectar directamente con tus futuros clientes, y un apartado de contactos para que puedan seguirte en tus redes sociales.
-            </p>
+            <ul className={styles.lista}>
+            <li><Image src={check}></Image>Formulario de Contacto</li>
+            <li><Image src={check}></Image>Multiple páginas</li>
+            <li><Image src={check}></Image>Whatsaap Personalizado</li>
+            <li><Image src={check}></Image>Responsive Desing</li>
+            <li><Image src={check}></Image>Dominio Personalizado</li>
+            <li><Image src={check}></Image>Contactos Redes Sociales</li>
+            </ul>
             <h2>$120000</h2>
         </div>
         <div className={styles.sitio}>
             <h1>Sitio Web</h1>
-            <p>Un Sitio Web es un sitio que ofrece multiples páginas ya sea páginas de productos o servicios, como páginas de presentaciones, blogs y contactos
-                un Sitio Web es un espacio en internet más robusto.
-                Contiene también un whatsaap adaptable a las distintas páginas para que el futuro cliente pueda conectarse directamente con un operador o persona a cargo de la atención al cliente
-            </p>
+            <ul className={styles.lista}>
+            <li><Image src={check}></Image>Formulario de Contacto</li>
+            <li><Image src={check}></Image>Multiple páginas</li>
+            <li><Image src={check}></Image>Whatsaap Personalizado</li>
+            <li><Image src={check}></Image>Responsive Desing</li>
+            <li><Image src={check}></Image>Dominio Personalizado</li>
+            <li><Image src={check}></Image>Contactos Redes Sociales</li>
+            </ul>
             <h2>$160000</h2>
         </div>
     </div>
